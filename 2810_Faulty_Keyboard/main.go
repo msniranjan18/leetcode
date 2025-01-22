7cd8fcb56c4cfd7843978301a561622289e2d1f7
Manvendra S. Niranjan <manvendraniranjan528@gmail.com>
2025-01-23 00:19:07 +0530

    2810. Faulty Keyboard

---
+++ 2810_Faulty_Keyboard/main.go
@@ -0,0 +1,109 @@
+/*
+Your laptop keyboard is faulty, and whenever you type a character 'i' on it, it reverses the string that you have written. Typing other characters works as expected.
+
+You are given a 0-indexed string s, and you type each character of s using your faulty keyboard.
+
+Return the final string that will be present on your laptop screen.
+
+
+
+Example 1:
+
+Input: s = "string"
+Output: "rtsng"
+Explanation:
+After typing first character, the text on the screen is "s".
+After the second character, the text is "st".
+After the third character, the text is "str".
+Since the fourth character is an 'i', the text gets reversed and becomes "rts".
+After the fifth character, the text is "rtsn".
+After the sixth character, the text is "rtsng".
+Therefore, we return "rtsng".
+Example 2:
+
+Input: s = "poiinter"
+Output: "ponter"
+Explanation:
+After the first character, the text on the screen is "p".
+After the second character, the text is "po".
+Since the third character you type is an 'i', the text gets reversed and becomes "op".
+Since the fourth character you type is an 'i', the text gets reversed and becomes "po".
+After the fifth character, the text is "pon".
+After the sixth character, the text is "pont".
+After the seventh character, the text is "ponte".
+After the eighth character, the text is "ponter".
+Therefore, we return "ponter".
+
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of lowercase English letters.
+s[0] != 'i'
+*/
+
+package main
+
+import "fmt"
+
+type TestCase struct {
+	GivenString string
+	Expected    string
+}
+
+func main() {
+	testCases := []TestCase{
+		{"string", "rtsng"},
+		{"poiinter", "ponter"},
+		{"poi", "op"},
+		{"poiinteri", "retnop"},
+		{"aiibiiciidiieiifii", "abcdef"},
+		{"aibicidieifi", "fdbace"},
+		{"aiiiiiii", "a"},
+	}
+
+	for _, testCase := range testCases {
+		result := finalString(testCase.GivenString)
+		if result == testCase.Expected {
+			fmt.Printf("Test Case: [GivenString=%+v] PASSED: output: %s, expected %s\n", testCase.GivenString, result, testCase.Expected)
+		} else {
+			fmt.Printf("Test Case: [GivenString=%+v] FAILED: output: %s, expected %s\n", testCase.GivenString, result, testCase.Expected)
+		}
+	}
+
+}
+
+func finalString(s string) string {
+	str := []byte(s)
+	for i := 0; i < len(str); i++ {
+		if string(str[i]) == "i" {
+			reverseStr(str[:i])
+			if i != len(str)-1 {
+				copy(str[i:], str[i+1:])
+			}
+			str = str[:len(str)-1]
+			i--
+		}
+	}
+	return string(str)
+}
+
+func reverseStr(s []byte) {
+	i, j := 0, len(s)-1
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
+	}
+}
+
+/*
+Output:
+Test Case: [GivenString=string] PASSED: output: rtsng, expected rtsng
+Test Case: [GivenString=poiinter] PASSED: output: ponter, expected ponter
+Test Case: [GivenString=poi] PASSED: output: op, expected op
+Test Case: [GivenString=poiinteri] PASSED: output: retnop, expected retnop
+Test Case: [GivenString=aiibiiciidiieiifii] PASSED: output: abcdef, expected abcdef
+Test Case: [GivenString=aibicidieifi] PASSED: output: fdbace, expected fdbace
+Test Case: [GivenString=aiiiiiii] PASSED: output: a, expected a
+*/
