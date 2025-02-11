40776f58ae3b86b61132c5a33d9037f6872c382d
Manvendra S. Niranjan <manvendraniranjan528@gmail.com>
2025-02-11 23:07:30 +0530

    1732. Find the Highest Altitude

---
+++ 1732. Find the Highest Altitude/main.go
@@ -0,0 +1,54 @@
+/*
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+Example 1:
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+Example 2:
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+
+Constraints:
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	testcases := [][]int{
+		{-5, 1, 5, 0, -7},
+		{-4, -3, -2, -1, 4, 3, 2},
+	}
+	for _, testcase := range testcases {
+		fmt.Printf("Testcase:%+v, result:%v\n", testcase, largestAltitude(testcase))
+	}
+
+}
+
+func largestAltitude(gain []int) int {
+	maxAltitude := 0
+	sum := 0
+	for i := 0; i < len(gain); i++ {
+		sum += gain[i]
+		if sum > maxAltitude {
+			maxAltitude = sum
+		}
+	}
+	return maxAltitude
+}
+
+/*
+Output:
+Testcase:[-5 1 5 0 -7], result:1
+Testcase:[-4 -3 -2 -1 4 3 2], result:0
+*/
