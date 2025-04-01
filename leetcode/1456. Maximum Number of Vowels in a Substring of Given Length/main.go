03bef88d941e7e7006de7ee8871bc94e085058b0
Manvendra S. Niranjan <manvendraniranjan528@gmail.com>
2025-04-01 07:17:44 +0530

    1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ leetcode/1456. Maximum Number of Vowels in a Substring of Given Length/main.go
@@ -0,0 +1,67 @@
+/*
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+Example 1:
+
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= s.length
+*/
+
+
+func maxVowels(s string, k int) int {
+    maxVowelCount := 0
+    count := 0
+
+    // count the vowel for first window
+    for i:=0; i<k; i++ {
+        if isVowel(s[i]) {
+            count++
+        }
+    }
+
+    maxVowelCount = count // set the max after first window count
+    
+    // now slide the window
+    for i:=k; i< len(s); i++ {
+        if isVowel(s[i-k]) { // Remove outgoing character
+            count--
+        }
+        if isVowel(s[i]) { // add incomming character
+            count++
+        }
+        if count > maxVowelCount {
+            maxVowelCount = count
+        }
+    }
+    return maxVowelCount
+}
+
+func isVowel(ch byte) bool {
+    switch ch {
+    case 'a', 'e', 'i', 'o', 'u':
+        return true
+    }
+    return false
+}
