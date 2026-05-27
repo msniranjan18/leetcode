29e181707af4554448146f83d29b292bee9a555c
msniranjan18 <manvendra45@outlook.com>
2026-05-27 04:16:41 +0000

    7 reverse number

--- 7-reverse-number/main.go
+++ 7-reverse-number/main.go
@@ -6,20 +6,29 @@ import (
 )
 
 func main() {
-	fmt.Println(reverseInt32(1534236469))
+	fmt.Println(reverse(1534236469))
 }
 
-func reverseInt32(x int) int {
-	if x > math.MaxInt32 || x < math.MinInt32 {
-		return 0
-	}
-	reversed := 0
-	for x != 0 {
-		reversed = reversed*10 + x%10
-		x /= 10
-	}
-	if reversed > math.MaxInt32 || reversed < math.MinInt32 {
-		return 0
-	}
-	return reversed
+func reverse(x int) int {
+   if x > math.MaxInt32 || x < math.MinInt32 {
+        return 0
+   }
+   
+   rev:=0
+   for x!=0 {
+    digit := x%10
+
+    if rev > math.MaxInt32/10 || rev == math.MaxInt32/10 && digit > 7 {
+        return 0
+    }
+    if rev < math.MinInt32/10 || rev == math.MinInt32/10 && digit < -8 {
+        return 0
+    }
+
+    //fmt.Println(x, digit, rev)
+
+    rev = rev * 10 + digit
+    x = x/10
+   }
+    return rev
 }
