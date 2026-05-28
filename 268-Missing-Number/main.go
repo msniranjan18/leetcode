be06ac224ae1f6552784efc2e349f316e32e6a3a
msniranjan18 <manvendra45@outlook.com>
2026-05-28 01:21:55 +0000

    268. missing number

--- 268-Missing-Number/main.go
+++ 268-Missing-Number/main.go
@@ -1,60 +1,27 @@
-/*
-Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+package main
 
- 
+import (
+   "fmt"
+)
 
-Example 1:
+func main() {
+   fmt.Println(missingNumber([]int{0,2}))
+   fmt.Println(missingNumber([]int{0,2,1,3,5}))
+   fmt.Println(missingNumber([]int{1}))
+}
 
-Input: nums = [3,0,1]
-
-Output: 2
-
-Explanation:
-
-n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
-
-Example 2:
-
-Input: nums = [0,1]
-
-Output: 2
-
-Explanation:
-
-n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
-
-Example 3:
-
-Input: nums = [9,6,4,2,3,5,7,0,1]
-
-Output: 8
-
-Explanation:
-
-n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
-
-
-
-Constraints:
-
-n == nums.length
-1 <= n <= 104
-0 <= nums[i] <= n
-All the numbers of nums are unique.
- 
-
-Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
-*/
 
 func missingNumber(nums []int) int {
    // AP sum formula
    // sum = n/2(2a+(n-1)d)
-    sum:=0
-    n := len(nums)+1
-   totalSum := (n*(n-1))/2
+   // where a is starting number and d is different 
+   // in this case a=0 and d=1
+   // so formula will be n/2(2*0+(n-1)*1) = n(n-1)/2
+   sum:=0
+   n := len(nums)+1
+   totalSum := n*(n-1)/2
    for _,num := range nums {
-    sum+=num
+    sum += num
    }
-   fmt.Println(sum)
    return totalSum-sum
 }
