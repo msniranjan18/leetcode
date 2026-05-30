4ecfb8a9738c73aed168ebf4530802343b1caea6
msniranjan18 <manvendra45@outlook.com>
2026-05-30 02:31:52 +0000

    7. reverse number

--- 7-reverse-number/main.go
+++ 7-reverse-number/main.go
@@ -10,25 +10,24 @@ func main() {
 }
 
 func reverse(x int) int {
-   if x > math.MaxInt32 || x < math.MinInt32 {
-        return 0
-   }
-   
-   rev:=0
-   for x!=0 {
-    digit := x%10
-
-    if rev > math.MaxInt32/10 || rev == math.MaxInt32/10 && digit > 7 {
-        return 0
-    }
-    if rev < math.MinInt32/10 || rev == math.MinInt32/10 && digit < -8 {
+    fmt.Println(math.MaxInt32, x ,math.MinInt32) // this is required to know the boundries
+    if x > math.MaxInt32 || x < math.MinInt32 {
         return 0
     }
 
-    //fmt.Println(x, digit, rev)
+    rev:=0
+    for x!=0 {
+        digit := x%10
 
-    rev = rev * 10 + digit
-    x = x/10
-   }
+        if rev > math.MaxInt32/10 || rev == math.MaxInt32/10 && digit > 7 {
+            return 0
+        }
+        if rev < math.MinInt32/10 || rev == math.MinInt32/10 && digit < -8 {
+            return 0
+        }
+
+        rev = rev * 10 + digit
+        x = x/10
+    }
     return rev
 }
